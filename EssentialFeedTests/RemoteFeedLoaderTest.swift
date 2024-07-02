f8135280351f43a66c77c61e8b8772a4170cf9ea
Juan Carlos Merlos Albarracin <merlosalbarracin@gmail.com>
2024-07-02 10:11:33 +0200

    “Make the HTTPClient aprotocol

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -8,19 +8,47 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "htpps://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTest: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
                 
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        // Arrange: Given a client and sut
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        // Act: When we invoke sut.load()
+        sut.load()
+        
+        // Assert: Then assert that a URL request was initiated in the client
+        XCTAssertNotNil(client.requestedURL)
     }
 }
