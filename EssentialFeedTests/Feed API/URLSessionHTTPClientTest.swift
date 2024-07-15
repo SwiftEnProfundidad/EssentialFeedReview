64987dc6ea626c7d22533197635143526e06fa0b
Juan Carlos Merlos Albarracin <merlosalbarracin@gmail.com>
2024-07-15 22:29:09 +0200

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -0,0 +1,46 @@
+//
+//  URLSessionHTTPClientTest.swift
+//  EssentialFeedTests
+//
+//  Created by Juan Carlos merlos albarracin on 15/7/24.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+  private let session: URLSession
+  
+  init(session: URLSession) {
+    self.session = session
+  }
+  
+  func get(from url: URL) {
+    session.dataTask(with: url) { _, _, _ in }
+  }
+}
+
+final class URLSessionHTTPClientTest: XCTestCase {
+
+  func test_getFromURL_createsDataTaskWithURL() {
+    let url = URL(string: "http://any-url.com")!
+    let session = URLSessionSpy()
+    let sut = URLSessionHTTPClient(session: session)
+    
+    sut.get(from: url)
+    
+    XCTAssertEqual(session.receivedURLs, [url])
+  }
+  
+  // MARK: Helpers
+  
+  private class URLSessionSpy: URLSession, @unchecked Sendable {
+    var receivedURLs = [URL]()
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+      receivedURLs.append(url)
+      return FakeURLSessionDataTask()
+    }
+  }
+
+  private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {}
+}
