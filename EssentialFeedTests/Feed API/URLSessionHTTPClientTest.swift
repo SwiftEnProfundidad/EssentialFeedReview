ce394382b1e4e789b7ab634c96884ca1f3d50d7b
Juan Carlos Merlos Albarracin <merlosalbarracin@gmail.com>
2024-07-15 22:57:18 +0200

    Resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class URLSessionHTTPClient {
+final class URLSessionHTTPClient {
   private let session: URLSession
   
   init(session: URLSession) {
@@ -15,7 +15,7 @@ class URLSessionHTTPClient {
   }
   
   func get(from url: URL) {
-    session.dataTask(with: url) { _, _, _ in }
+    session.dataTask(with: url) { _, _, _ in }.resume()
   }
 }
 
@@ -31,16 +31,45 @@ final class URLSessionHTTPClientTest: XCTestCase {
     XCTAssertEqual(session.receivedURLs, [url])
   }
   
+  func test_getFromURL_resumesDataTaskWithURL() {
+    let url = URL(string: "http://any-url.com")!
+    let session = URLSessionSpy()
+    let task = URLSessionDataTaskSpy()
+    session.stub(url: url, task: task)
+    
+    let sut = URLSessionHTTPClient(session: session)
+    
+    sut.get(from: url)
+    
+    XCTAssertEqual(task.resumeCallCount, 1)
+  }
+  
   // MARK: Helpers
   
   private class URLSessionSpy: URLSession, @unchecked Sendable {
     var receivedURLs = [URL]()
+    private var stubs = [URL: URLSessionDataTask]()
+
+    func stub(url: URL, task: URLSessionDataTask) {
+      stubs[url] = task
+    }
     
     override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
       receivedURLs.append(url)
-      return FakeURLSessionDataTask()
+      return stubs[url] ?? FakeURLSessionDataTask()
+
+    }
+  }
+  
+  private class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+    var resumeCallCount = 0
+    
+    override func resume() {
+      resumeCallCount += 1
     }
   }
 
-  private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {}
+  private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+    override func resume() {}
+  }
 }
