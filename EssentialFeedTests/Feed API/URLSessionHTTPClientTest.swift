944e85e5c4d735886efe6a6f209dd3dfc3c7fd94
Juan Carlos Merlos Albarracin <merlosalbarracin@gmail.com>
2024-07-20 21:01:27 +0200

    Guarantees correct URL and HTTP method on get requests

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -29,12 +29,12 @@ class URLSessionHTTPClientTests: XCTestCase {
   
   func test_getFromURL_failsOnRequestError() {
     URLProtocolStub.startInterceptingRequests()
+    
     let url = URL(string: "http://any-url.com")!
     let error = NSError(domain: "any error", code: 1)
     URLProtocolStub.stub(data: nil, response: nil, error: error)
     
     let sut = URLSessionHTTPClient()
-    
     let exp = expectation(description: "Wait for completion")
     
     sut.get(from: url) { result in
@@ -58,11 +58,13 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     let url = URL(string: "http://any-url.com")!
     let exp = expectation(description: "Wait for request")
+    
     URLProtocolStub.observerRequest { request in
       XCTAssertEqual(request.url, url)
       XCTAssertEqual(request.httpMethod, "GET")
       exp.fulfill()
     }
+    
     URLSessionHTTPClient().get(from: url) { _ in }
     wait(for: [exp], timeout: 1.0)
 
