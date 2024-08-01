5ac39687595b1f1ec7cd213054cdea0f7fb084a3
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-01 19:18:16 +0200

    Use ephemeral URL session configuration to avoid sharing state across test executions (in-disk cache artifacts)

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -35,7 +35,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
   
   private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
     let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-    let client = URLSessionHTTPClient()
+    let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     let loader = RemoteFeedLoader(url: testServerURL, client: client)
     trackForMemoryLeak(client, file: file, line: line)
     trackForMemoryLeak(loader, file: file, line: line)
--- EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTest.swift
@@ -190,11 +190,19 @@ final class RemoteFeedLoaderTest: XCTestCase {
       messages.append((url, completion))
     }
     
-    func complete(with error: Error, at index: Int = 0) {
+    func complete(with error: Error, at index: Int = 0, file: StaticString = #filePath, line: UInt = #line) {
+      guard messages.count > index else {
+        return XCTFail("Can't complete request never made", file: file, line: line)
+      }
+      
       messages[index].completion(.failure(error))
     }
     
-    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0, file: StaticString = #filePath, line: UInt = #line) {
+      guard requestedURLs.count > index else {
+        return XCTFail("Can't complete request never made", file: file, line: line)
+      }
+      
       let response = HTTPURLResponse(
         url: requestedURLs[index],
         statusCode: code,
