081b3b1061cf1ee1aa1a9f9d5cf8b0217ed37694
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-07 01:42:19 +0200

    Move FeedStore to its own file

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+public protocol FeedStore {
+  typealias DeletionCompletion = (Error?) -> Void
+  typealias InsertionCompletion = (Error?) -> Void
+  
+  func deleteCachedFeed(completion: @escaping DeletionCompletion)
+  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,14 +7,6 @@
 
 import Foundation
 
-public protocol FeedStore {
-  typealias DeletionCompletion = (Error?) -> Void
-  typealias InsertionCompletion = (Error?) -> Void
-  
-  func deleteCachedFeed(completion: @escaping DeletionCompletion)
-  func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
-}
-
 public final class LocalFeedLoader {
   private let store: FeedStore
   private let currentDate: () -> Date
