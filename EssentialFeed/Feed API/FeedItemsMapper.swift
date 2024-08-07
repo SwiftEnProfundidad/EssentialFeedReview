eefdc7d9453f1ac90ff0f2e04a18abf0888bac64
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-07 10:19:38 +0200

    Add RemoteFeedItem data transfer representation to decouple the items mapper from `FeedItem` data models

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,37 +7,25 @@
 
 import Foundation
 
-class FeedItemMapper {
-    private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            return items.map { $0.item }
-        }
+internal struct RemoteFeedItem: Decodable {
+  internal let id: UUID
+  internal let description: String?
+  internal let location: String?
+  internal let image: URL
+}
+
+internal final class FeedItemMapper {
+  private struct Root: Decodable {
+    let items: [RemoteFeedItem]
+  }
+  
+  static var OK_200: Int { return 200 }
+  
+  internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+    guard response.statusCode == OK_200,
+            let root = try? JSONDecoder().decode(Root.self, from: data) else {
+      throw RemoteFeedLoader.Error.invalidData
     }
-    
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            FeedItem(
-                id: id,
-                description: description,
-                location: location,
-                imageURL: image)
-        }
-    }
-    
-    static var OK_200: Int { return 200 }
-    
-    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == OK_200, 
-                let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
-        }
-        return .success(root.feed)
-    }
+    return root.items
+  }
 }
--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,31 +8,49 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-    private let url: URL
-    private let client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
+  private let url: URL
+  private let client: HTTPClient
+  
+  public enum Error: Swift.Error {
+    case connectivity
+    case invalidData
+  }
+  
+  public typealias Result = LoadFeedResult
+  
+  public init(url: URL, client: HTTPClient) {
+    self.url = url
+    self.client = client
+  }
+  
+  public func load(completion: @escaping (Result) -> Void) {
+    client.get(from: url) { [weak self] result in
+      guard self != nil else { return }
+      switch result {
+        case let .success(data, response):
+          completion(RemoteFeedLoader.map(data, from: response))
+        case .failure:
+          completion(.failure(Error.connectivity))
+      }
     }
-    
-    public typealias Result = LoadFeedResult
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
+  }
+  
+  private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+    do {
+      let items = try FeedItemMapper.map(data, from: response)
+      return .success(items.toModels())
+    } catch {
+      return .failure(error)
     }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-                case let .success(data, response):
-                    completion(FeedItemMapper.map(data, from: response))
-                case .failure:
-                    completion(.failure(Error.connectivity))
-            }
-        }
-    }
+  }
+}
+
+private extension Array where Element == RemoteFeedItem {
+  func toModels() -> [FeedItem] {
+    return self.map { FeedItem(id: $0.id,
+                          description: $0.description,
+                          location: $0.location,
+                          imageURL: $0.image) }
+  }
 }
 
