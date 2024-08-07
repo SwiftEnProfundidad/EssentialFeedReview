286fa91173c3f7b9789aecb794d38db04408cd1f
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-07 18:56:13 +0200

    Extract duplicate test code into a shared helper method

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -27,44 +27,19 @@ final class LoadFeedFromCacheUseCaseTest: XCTestCase {
   func test_load_failsOnRetrievalError() {
     let (sut, store) = makeSUT()
     let retrievalError = anyNSError()
-    let exp = expectation(description: "Wait for load completion")
     
-    var receivedError: Error?
-    sut.load { result in
-      switch result {
-        case let .failure(error):
-          receivedError = error
-        default:
-          XCTFail("Expected failure, got \(String(describing: result)) instead", file: #file, line: #line)
-      }
-      exp.fulfill()
+    expect(sut, toCompleteWith: .failure(retrievalError)) {
+      store.completeRetrieval(with: retrievalError)
     }
-    store.completeRetrieval(with: retrievalError)
-    wait(for: [exp], timeout: 1.0)
-    
-    XCTAssertEqual(receivedError as NSError?, retrievalError)
   }
   
-    func test_load_deliversNoImagesOnEmptyCache() {
-      let (sut, store) = makeSUT()
-  
-      let exp = expectation(description: "Wait for load completion")
-      var receivedImages: [FeedImage]?
-  
-      sut.load { result in
-        switch result {
-          case let .success(images):
-            receivedImages = images
-          default:
-            XCTFail("Expected success, got \(String(describing: result)) instead", file: #file, line: #line)
-        }
-          exp.fulfill()
-      }
+  func test_load_deliversNoImagesOnEmptyCache() {
+    let (sut, store) = makeSUT()
+    
+    expect(sut, toCompleteWith:.success([])) {
       store.completeRetrievalWithEmptyCache()
-      wait(for: [exp], timeout: 1.0)
-  
-      XCTAssertEqual(receivedImages, [])
     }
+  }
   
   // MARK: - Helpers
   
@@ -79,7 +54,31 @@ final class LoadFeedFromCacheUseCaseTest: XCTestCase {
       return (sut, store)
     }
   
-  
+  func expect(
+    _ sut: LocalFeedLoader,
+    toCompleteWith expectedResult: LocalFeedLoader.LoadResult,
+    when action: () -> Void,
+    file: StaticString = #filePath, line: UInt = #line) {
+    let exp = expectation(description: "Wait for load to completion")
+    
+    sut.load { receivedResult in
+      switch (receivedResult, expectedResult) {
+        case let (.success(receivedImages), .success(expectedImages)):
+          XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
+          
+        case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+          XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+          
+        default:
+          XCTFail("Expected \(expectedResult), got \(String(describing: receivedResult)) instead", file: file, line: line)
+      }
+      exp.fulfill()
+    }
+    
+    // En lugar de invocar un método a la `store` directamente, invocamos la acción
+    action()
+    wait(for: [exp], timeout: 1.0)
+  }
   
   private func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
