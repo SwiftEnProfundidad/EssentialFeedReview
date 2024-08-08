ef8d33ef4fb4dadd8d75a7acba5ee36aab4ba599
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-08 18:38:33 +0200

    Move FeedCachePolicy to its own file abd internal scope

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-final class FeedCachePolicy {
+internal final class FeedCachePolicy {
   static private let calendar = Calendar(identifier: .gregorian)
   static private var maxCacheAgeInDays: Int { return 7 }
   
   private init() {}
   
-  static func validate(_ timestamp: Date, against date: Date) -> Bool {
+  internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
     guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
       return false
     }
