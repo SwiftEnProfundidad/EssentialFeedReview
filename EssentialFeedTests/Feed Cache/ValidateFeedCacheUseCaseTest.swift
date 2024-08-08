487a506592e2487b146521ed9fae0d10bf26b1ba
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-08 12:23:17 +0200

    Validate cache command does not delete less than seven days old cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
@@ -43,6 +43,17 @@ final class ValidateFeedCacheUseCaseTest: XCTestCase {
     XCTAssertEqual(store.receivedMessages, [.retrieve])
   }
   
+  func test_validateCache_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+    let feeds = uniqueImageFeeds()
+    let fixedCurrentDate = Date()
+    let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+    let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+    
+    sut.validateCache()
+    store.completeRetrieval(with: feeds.local, timestamp: lessThanSevenDaysOldTimestamp)
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve])
+  }
   // MARK: - Helpers
   
   private func makeSUT(
@@ -56,8 +67,40 @@ final class ValidateFeedCacheUseCaseTest: XCTestCase {
     return (sut, store)
   }
   
+  private func uniqueImageFeeds() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueFeed(), uniqueFeed()]
+    let local = models.map { LocalFeedImage(
+      id: $0.id,
+      description: $0.description,
+      location: $0.location,
+      url: $0.url) }
+    return (models, local)
+  }
+  
+  private func uniqueFeed() -> FeedImage {
+    return FeedImage(
+      id: UUID(),
+      description: "any",
+      location: "any",
+      url: anyURL())
+  }
+  
   private func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
   }
   
+  private func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+  }
+  
+}
+
+private extension Date {
+  func adding(days: Int) -> Date {
+    return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+  }
+  
+  func adding(seconds: TimeInterval) -> Date {
+    return self + seconds
+  }
 }
