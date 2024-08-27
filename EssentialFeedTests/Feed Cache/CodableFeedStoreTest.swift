6159e237c5e5be487fc9f8dc17a219ecef30bf0c
Swift En Profundidad <freelancemerlos@gmail.com>
2024-08-27 11:54:27 +0200

    Move Codable conformance from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFeedImage` type. The `CodableFeedImage` is a private type within the framework implementation since the `Codable` requirement is a framework-specific detail.

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTest.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTest.swift
@@ -10,8 +10,34 @@ import EssentialFeed
 
 class CodableFeedStore {
   private struct Cache: Codable {
-    let feed: [LocalFeedImage]
+    let feed: [CodableFeedImage]
     let timestamp: Date
+    
+    var localFeeds: [LocalFeedImage] {
+      return feed.map { $0.local }
+    }
+  }
+  
+  private struct CodableFeedImage: Codable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+    
+    init(_ image: LocalFeedImage) {
+      id = image.id
+      description = image.description
+      location = image.location
+      url = image.url
+    }
+    
+    var local: LocalFeedImage {
+      return LocalFeedImage(
+        id: id,
+        description: description,
+        location: location,
+        url: url)
+    }
   }
   
   private let storeURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
@@ -23,12 +49,13 @@ class CodableFeedStore {
     
     let decoder = JSONDecoder()
     let cache = try! decoder.decode(Cache.self, from: data)
-    completion(.found(feeds: cache.feed, timestamp: cache.timestamp))
+    completion(.found(feeds: cache.localFeeds, timestamp: cache.timestamp))
   }
   
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
     let encoder = JSONEncoder()
-    let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+    let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+    let encoded = try! encoder.encode(cache)
     try! encoded.write(to: storeURL)
     completion(nil)
   }
