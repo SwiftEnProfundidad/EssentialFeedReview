6a10df247e74b06e438ad303bf399decdd468740
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-09 21:31:35 +0200

    Replace custom HTTPClientResult enum with a nested typealias over the standard `Swift.Result`

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTest.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTest.swift
@@ -181,12 +181,12 @@ final class LoadFeedFromRemoteUseCaseTest: XCTestCase {
   }
   
   class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
     
     var requestedURLs: [URL] {
       return messages.map { $0.url }
     }
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
       messages.append((url, completion))
     }
     
@@ -208,7 +208,7 @@ final class LoadFeedFromRemoteUseCaseTest: XCTestCase {
         statusCode: code,
         httpVersion: nil,
         headerFields: nil)!
-      messages[index].completion(.success(data, response))
+      messages[index].completion(.success((data, response)))
     }
   }
 }
--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -92,12 +92,12 @@ class URLSessionHTTPClientTests: XCTestCase {
     return sut
   }
   
-  private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) throws -> HTTPClientResult {
+  private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #file, line: UInt = #line) throws -> HTTPClient.Result {
     URLProtocolStub.stub(data: data, response: response, error: error)
     let sut = makeSUT(file: file, line: line)
     let exp = expectation(description: "Wait for completion")
     
-    var receivedResult: HTTPClientResult!
+    var receivedResult: HTTPClient.Result!
     sut.get(from: anyURL()) { result in
       receivedResult = result
       exp.fulfill()
