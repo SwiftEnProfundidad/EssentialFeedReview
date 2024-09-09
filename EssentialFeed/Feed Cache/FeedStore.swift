d8886ae11b4d36a0a23a1018f198805381b2c8e1
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-09 23:23:37 +0200

    Simplify CoreDatafeedStore completion code with the new Result APIs

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -25,7 +25,7 @@ public protocol FeedStore {
   
   /// The completion handler can be invoked in any thread.
   /// Clients are responsible to dispatch to appropriate threads, if needed.
-  func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+  func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
   
   /// The completion handler can be invoked in any thread.
   /// Clients are responsible to dispatch to appropriate threads, if needed.
