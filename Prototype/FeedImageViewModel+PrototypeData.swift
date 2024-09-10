3bab49fab6bd44720f491f45b9ccaf5e42449b67
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-10 07:42:12 +0200

    Add FeedImageCell with storyboard connections

--- Prototype/FeedImageViewModel+PrototypeData.swift
+++ Prototype/FeedImageViewModel+PrototypeData.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension FeedImageViewModel {
-  static var prototypeNews: [FeedImageViewModel] {
+  static var prototypeFeed: [FeedImageViewModel] {
     return [
       FeedImageViewModel(
         description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
