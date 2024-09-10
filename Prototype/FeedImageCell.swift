0c0f0c06d066f72c9afba51b37eb2d05e449b2a3
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-10 10:22:44 +0200

    Render prototype news data on the table view

--- Prototype/FeedImageCell.swift
+++ Prototype/FeedImageCell.swift
@@ -10,6 +10,7 @@ import UIKit
 final class FeedImageCell: UITableViewCell {
   @IBOutlet private(set) var locationContainer: UIView!
   @IBOutlet private(set) var locationLabel: UILabel!
+  @IBOutlet private(set) var feedImageContainer: UIView!
   @IBOutlet private(set) var feedImageView: UIImageView!
   @IBOutlet private(set) var descriptionLabel: UILabel!
 }
--- Prototype/FeedViewController.swift
+++ Prototype/FeedViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 final class FeedViewController: UITableViewController {
+  private let feed = FeedImageViewModel.prototypeFeed
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return feed.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+    let model = feed[indexPath.row]
+    cell.configure(with: model)
+    return cell
+  }
+}
+
+extension FeedImageCell {
+  func configure(with viewModel: FeedImageViewModel) {
+    locationLabel.text = viewModel.location
+    locationContainer.isHidden = viewModel.location == nil
+    
+    descriptionLabel.text = viewModel.description
+    descriptionLabel.isHidden = viewModel.description == nil
+    
+    feedImageView.image = UIImage(named: viewModel.imageName)
   }
 }
