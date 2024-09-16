9d1cb9e59210fb1a2434f6d7a2ad7c6fe38f0887
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-16 11:24:38 +0200

    Render loaded images from URL

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -72,8 +72,11 @@ public final class FeedViewController: UITableViewController {
     cell.locationContainer.isHidden = (cellModel.location == nil)
     cell.locationLabel.text = cellModel.location
     cell.descriptionLabel.text = cellModel.description
+    cell.feedImageView.image = nil
     cell.feedImageViewContainer.startShimmering()
     task[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+      let data = try? result.get()
+      cell?.feedImageView.image = data.map(UIImage.init) ?? nil
       cell?.feedImageViewContainer.stopShimmering()
     }
     return cell
