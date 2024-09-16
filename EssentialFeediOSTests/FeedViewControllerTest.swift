2dd278e1b78f649bae3cae4c0da5613725fda3c1
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-16 11:43:42 +0200

    Feed image view retry button is visible on image url load error

--- EssentialFeediOSTests/FeedViewControllerTest.swift
+++ EssentialFeediOSTests/FeedViewControllerTest.swift
@@ -147,7 +147,7 @@ final class FeedViewControllerTest: XCTestCase {
     XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
   }
   
-  func test_newsImageViewRetryButton_isVisibleOnImageURLLoadError() {
+  func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
     let (sut, loader) = makeSUT()
     
     sut.simulateAppearance()
@@ -167,6 +167,21 @@ final class FeedViewControllerTest: XCTestCase {
     XCTAssertEqual(view0?.isShowingRetryAction, false, "Expected no retry action state change for first view once second image loading complete with error")
     XCTAssertEqual(view1?.isShowingRetryAction, true, "Expected retry action for second view once second image loading completes with error")
   }
+  
+  func test_feedImageViewRetryButton_isVisibleOnInvalidImageData() {
+    let (sut, loader) = makeSUT()
+    
+    sut.simulateAppearance()
+    loader.completeFeedLoading(with: [makeImage()])
+    
+    let view = sut.simulateFeedImageViewVisible(at: 0)
+    XCTAssertEqual(view?.isShowingRetryAction, false, "Expected no retry action while loading image")
+    
+    let invalidImageData = Data("invalid image data".utf8)
+    loader.completeImageLoading(with: invalidImageData, at: 0)
+    XCTAssertEqual(view?.isShowingRetryAction, true, "Expected retry action once image loading completes with invalid image data")
+  }
+
   
   // MARK: - Helpers
   
