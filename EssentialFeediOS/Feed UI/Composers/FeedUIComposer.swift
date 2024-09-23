0f5ecb448404eb9b8b1b1a05dde0e88173b41bf9
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-23 21:07:06 +0200

    Move memory management to Composer layer. The FeedPresenter shouldn't have to know or handle their dependencies lifetime

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -16,9 +16,8 @@ public final class FeedUIComposer {
     let presenter = FeedPresenter(feedLoader: feedLoader)
     let refreshController = FeedRefreshViewController(presenter: presenter)
     let feedController = FeedViewController(refreshController: refreshController)
-    
+    presenter.loadingView = WeakRefVirtualProxy(refreshController)
     presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
-    
     return feedController
   }
   
@@ -30,29 +29,34 @@ public final class FeedUIComposer {
       }
     }
   }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+  weak var objetct: T?
   
-  private final class FeedViewAdapter: FeedView {
-    private weak var controller: FeedViewController?
-    private let imageLoader: FeedImageDataLoader
-    
-    init(controller: FeedViewController? = nil, imageLoader: FeedImageDataLoader) {
-      self.controller = controller
-      self.imageLoader = imageLoader
-    }
-    
-    func display(feed: [FeedImage]) {
-      controller?.tableModel = feed.map { model in
-        FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
-      }
-    }
-    
-    // [FeedImage] --> Adapter --> [FeedImageCellController]
-    private static func adapterFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
-      return { [weak controller] feed in
-        controller?.tableModel = feed.map { model in
-          FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
-        }
-      }
+  init(_ objetct: T) {
+    self.objetct = objetct
+  }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+  func display(isLoading: Bool) {
+    objetct?.display(isLoading: isLoading)
+  }
+}
+
+private final class FeedViewAdapter: FeedView {
+  private weak var controller: FeedViewController?
+  private let imageLoader: FeedImageDataLoader
+  
+  init(controller: FeedViewController? = nil, imageLoader: FeedImageDataLoader) {
+    self.controller = controller
+    self.imageLoader = imageLoader
+  }
+  
+  func display(feed: [FeedImage]) {
+    controller?.tableModel = feed.map { model in
+      FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
     }
   }
 }
