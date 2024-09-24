e2a94eb0b1e0bc29eafcd059ac3d4d3de0a8c29a
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-24 18:40:02 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Juan Carlos merlos albarracin on 24/9/24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+  let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Juan Carlos merlos albarracin on 24/9/24.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+  let feed: [FeedImage]
+}
