6eb661fd0d0fb674cb00f676c43088d5b32dec67
Swift En Profundidad <freelancemerlos@gmail.com>
2024-09-25 06:43:52 +0200

    Ensure previous cells' prepareForReuse doesn't affect new cells by removing the onReuse closure reference when releasing the cell for reuse

--- EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -247,7 +247,22 @@ final class FeedViewControllerTests: XCTestCase {
     loader.completeImageLoading(with: anyImageData())
     
     XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+  }
+  
+  func test_feedImageView_showsDataForNewViewRequestAfterPreviousViewIsReused() throws {
+    let (sut, loader) = makeSUT()
     
+    sut.simulateAppearance()
+    loader.completeFeedLoading(with: [makeImage(), makeImage()])
+    
+    let previousView = try XCTUnwrap(sut.simulateFeedImageViewNotVisible(at: 0))
+    let newView = try XCTUnwrap(sut.simulateFeedImageViewVisible(at: 0))
+    previousView.prepareForReuse()
+    
+    let imageData = UIImage.make(withColor: .red).pngData()!
+    loader.completeImageLoading(with: imageData, at: 1)
+    
+    XCTAssertEqual(newView.renderedImage, imageData)
   }
   
   // MARK: - Helpers
