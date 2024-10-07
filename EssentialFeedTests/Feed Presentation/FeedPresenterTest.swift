4026064b24880951836d652f03e27a0f817f7139
Swift En Profundidad <freelancemerlos@gmail.com>
2024-10-07 15:32:19 +0200

    FeedPresenter displays no error on didStartLoadingFeed

--- EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
@@ -7,9 +7,27 @@
 
 import XCTest
 
+struct FeedErrorViewModel {
+  let errorMessage: String?
+  
+  static var noError: FeedErrorViewModel {
+    return FeedErrorViewModel(errorMessage: nil)
+  }
+}
+
+protocol FeedErrorView {
+  func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
-  init(view: Any) {
-    
+  private var errorView: FeedErrorView
+  
+  init(errorView: FeedErrorView) {
+    self.errorView = errorView
+  }
+  
+  func didStartLoadingFeed() {
+    errorView.display(.noError)
   }
 }
 
@@ -18,22 +36,40 @@ final class FeedPresenterTest: XCTestCase {
   func test_init_doesNotSendMessagegesToView() {
     let (_, view) = makeSUT()
     
-    _ = FeedPresenter(view: view)
+    _ = FeedPresenter(errorView: view)
     
     XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+  }
+  
+  func test_didStartLoadingFeed_displayNoErrorMessages() {
+    let (sut, view) = makeSUT()
+    
+    sut.didStartLoadingFeed()
+    
+    XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
   }
   
   // MARK: - Helpers
   
   private func makeSUT() -> (sut: FeedPresenter, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = FeedPresenter(view: view)
+    let sut = FeedPresenter(errorView: view)
     trackForMemoryLeaks(sut, file: #file, line: #line)
     trackForMemoryLeaks(view, file: #file, line: #line)
     return (sut, view)
   }
   
-  private class ViewSpy {
-    var messages = [Any]()
+  private class ViewSpy: FeedErrorView {
+    enum Message: Equatable {
+      case display(errorMessage: String?)
+    }
+    
+    private(set) var messages = [Message]()
+    
+    func display(_ viewModel: FeedErrorViewModel) {
+      messages.append(.display(errorMessage: viewModel.errorMessage))
+    }
   }
+  
+  
 }
