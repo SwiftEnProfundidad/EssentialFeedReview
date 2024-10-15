e8a8d291e4ea971e4178dd556d4f4735f7221576
Swift En Profundidad <freelancemerlos@gmail.com>
2024-10-15 12:54:23 +0200

    Set SceneDelegate

--- EssentialFeedApp/SceneDelegate.swift
+++ EssentialFeedApp/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,8 +19,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let _ = (scene as? UIWindowScene) else { return }
+    
+    let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+    
+    let session = URLSession(configuration: .ephemeral)
+    let client = URLSessionHTTPClient(session: session)
+    let feedLoader = RemoteFeedLoader(url: url, client: client)
+    let imageLoader = RemoteFeedImageDataLoader(client: client)
+    
+    let feedViewController = FeedUIComposer.feedComposeWith(
+      feedLoader: feedLoader,
+      imageLoader: imageLoader)
+    
+    window?.rootViewController = feedViewController
   }
-
+  
   func sceneDidDisconnect(_ scene: UIScene) {
     // Called as the scene is being released by the system.
     // This occurs shortly after the scene enters the background, or when its session is discarded.
